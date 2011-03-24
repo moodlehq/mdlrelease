79a6f64fbd600f6f806cbd090d4eeff80dcef14a
Petr Skoda <commits@skodak.org>
2011-03-24 18:47:42 +0100

    use full target branch refs

--- gitmirror.sh
+++ gitmirror.sh
@@ -8,23 +8,23 @@
 cd gitmirror
 git fetch origin
 
-git push --tags moodle refs/remotes/origin/master:master
-git push --tags moodle refs/remotes/origin/MOODLE_20_STABLE:MOODLE_20_STABLE
-git push --tags moodle refs/remotes/origin/MOODLE_19_STABLE:MOODLE_19_STABLE
-git push --tags moodle refs/remotes/origin/MOODLE_18_STABLE:MOODLE_18_STABLE
-git push --tags moodle refs/remotes/origin/MOODLE_17_STABLE:MOODLE_17_STABLE
-git push --tags moodle refs/remotes/origin/MOODLE_16_STABLE:MOODLE_16_STABLE
+git push --tags moodle refs/remotes/origin/master:refs/heads/master
+git push --tags moodle refs/remotes/origin/MOODLE_20_STABLE:refs/heads/MOODLE_20_STABLE
+git push --tags moodle refs/remotes/origin/MOODLE_19_STABLE:refs/heads/MOODLE_19_STABLE
+git push --tags moodle refs/remotes/origin/MOODLE_18_STABLE:refs/heads/MOODLE_18_STABLE
+git push --tags moodle refs/remotes/origin/MOODLE_17_STABLE:refs/heads/MOODLE_17_STABLE
+git push --tags moodle refs/remotes/origin/MOODLE_16_STABLE:refs/heads/MOODLE_16_STABLE
 
-git push --tags github refs/remotes/origin/master:master
-git push --tags github refs/remotes/origin/MOODLE_20_STABLE:MOODLE_20_STABLE
-git push --tags github refs/remotes/origin/MOODLE_19_STABLE:MOODLE_19_STABLE
-git push --tags github refs/remotes/origin/MOODLE_18_STABLE:MOODLE_18_STABLE
-git push --tags github refs/remotes/origin/MOODLE_17_STABLE:MOODLE_17_STABLE
-git push --tags github refs/remotes/origin/MOODLE_16_STABLE:MOODLE_16_STABLE
+git push --tags github refs/remotes/origin/master:refs/heads/master
+git push --tags github refs/remotes/origin/MOODLE_20_STABLE:refs/heads/MOODLE_20_STABLE
+git push --tags github refs/remotes/origin/MOODLE_19_STABLE:refs/heads/MOODLE_19_STABLE
+git push --tags github refs/remotes/origin/MOODLE_18_STABLE:refs/heads/MOODLE_18_STABLE
+git push --tags github refs/remotes/origin/MOODLE_17_STABLE:refs/heads/MOODLE_17_STABLE
+git push --tags github refs/remotes/origin/MOODLE_16_STABLE:refs/heads/MOODLE_16_STABLE
 
-git push --tags gitorious refs/remotes/origin/master:master
-git push --tags gitorious refs/remotes/origin/MOODLE_20_STABLE:MOODLE_20_STABLE
-git push --tags gitorious refs/remotes/origin/MOODLE_19_STABLE:MOODLE_19_STABLE
-git push --tags gitorious refs/remotes/origin/MOODLE_18_STABLE:MOODLE_18_STABLE
-git push --tags gitorious refs/remotes/origin/MOODLE_17_STABLE:MOODLE_17_STABLE
-git push --tags gitorious refs/remotes/origin/MOODLE_16_STABLE:MOODLE_16_STABLE
+git push --tags gitorious refs/remotes/origin/master:refs/heads/master
+git push --tags gitorious refs/remotes/origin/MOODLE_20_STABLE:refs/heads/MOODLE_20_STABLE
+git push --tags gitorious refs/remotes/origin/MOODLE_19_STABLE:refs/heads/MOODLE_19_STABLE
+git push --tags gitorious refs/remotes/origin/MOODLE_18_STABLE:refs/heads/MOODLE_18_STABLE
+git push --tags gitorious refs/remotes/origin/MOODLE_17_STABLE:refs/heads/MOODLE_17_STABLE
+git push --tags gitorious refs/remotes/origin/MOODLE_16_STABLE:refs/heads/MOODLE_16_STABLE
