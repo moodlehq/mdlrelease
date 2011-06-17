aa5df84a74aa0adeaf7f0a35cc0db75f58803abb
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2011-06-17 12:11:32 +0200

    make diff CR-insensitive, as far as fix_newlines() is used by git_to_cvs_mirror.php

--- checkcvsmirror.sh
+++ checkcvsmirror.sh
@@ -5,21 +5,21 @@ cd gitmirror
 git checkout master
 git merge refs/remotes/origin/master
 cd ..
-diff -ru -I '\$\(Id\|Revision\|RCSfile\|Author\|Date\)[:$]' -x CVS -x .git gitmirror cvsmoodle
+diff -ru -I '\$\(Id\|Revision\|RCSfile\|Author\|Date\)[:$]' -x CVS -x .git --strip-trailing-cr gitmirror cvsmoodle
 
 # 2.0 branch
 cd gitmirror
 git checkout MOODLE_20_STABLE
 git merge refs/remotes/origin/MOODLE_20_STABLE
 cd ..
-diff -ru -I '\$\(Id\|Revision\|RCSfile\|Author\|Date\)[:$]' -x CVS -x .git gitmirror cvsmoodle20
+diff -ru -I '\$\(Id\|Revision\|RCSfile\|Author\|Date\)[:$]' -x CVS -x .git --strip-trailing-cr gitmirror cvsmoodle20
 
 # 1.9 branch
 cd gitmirror
 git checkout MOODLE_19_STABLE
 git merge refs/remotes/origin/MOODLE_19_STABLE
 cd ..
-diff -ru -I '\$\(Id\|Revision\|RCSfile\|Author\|Date\)[:$]' -x CVS -x .git gitmirror cvsmoodle19
+diff -ru -I '\$\(Id\|Revision\|RCSfile\|Author\|Date\)[:$]' -x CVS -x .git --strip-trailing-cr gitmirror cvsmoodle19
 
 # and back to master
 cd gitmirror
