9179477e8738a956fa3e17b53790069881266767
Sam Hemelryk <sam@moodle.com>
2013-11-29 14:33:39 +1300

    Removed the security branch from the weekly releases

--- prerelease.sh
+++ prerelease.sh
@@ -31,7 +31,8 @@ _nocreate=false
 localbuffer=""
 
 # Try to observe the "master first, then stables from older to newer" rule.
-weeklybranches=("master" "MOODLE_23_STABLE" "MOODLE_24_STABLE" "MOODLE_25_STABLE" "MOODLE_26_STABLE");
+# We don't make a weekly release of the security only branch any more. It is however still released during a minor release.
+weeklybranches=("master" "MOODLE_24_STABLE" "MOODLE_25_STABLE" "MOODLE_26_STABLE");
 minorbranches=("MOODLE_23_STABLE" "MOODLE_24_STABLE" "MOODLE_25_STABLE" "MOODLE_26_STABLE");
 majorbranches=("master");
 betabranches=("master");
