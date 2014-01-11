7dbf224d655b2efa208dde5d28affcaa510957b2
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2014-01-11 17:23:28 +0100

    NOBUG: 19 and 23 out. 24 to security.

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 # This scripts propagates changes from official integration Moodle git repository
 # to main repostiory and mirrors
@@ -150,12 +150,12 @@ fi
 git push ${pushargs} public refs/remotes/origin/master:refs/heads/master \
                             refs/remotes/origin/MOODLE_26_STABLE:refs/heads/MOODLE_26_STABLE \
                             refs/remotes/origin/MOODLE_25_STABLE:refs/heads/MOODLE_25_STABLE \
-                            refs/remotes/origin/MOODLE_24_STABLE:refs/heads/MOODLE_24_STABLE \
-                            refs/remotes/origin/MOODLE_23_STABLE:refs/heads/MOODLE_23_STABLE \
-                            refs/remotes/origin/MOODLE_19_STABLE:refs/heads/MOODLE_19_STABLE
-# Discontinued 20130708 -   refs/remotes/origin/MOODLE_22_STABLE:refs/heads/MOODLE_22_STABLE \
-# Discontinued 20130114 -   refs/remotes/origin/MOODLE_21_STABLE:refs/heads/MOODLE_21_STABLE \
-# Discontinued 20120706 -   refs/remotes/origin/MOODLE_20_STABLE:refs/heads/MOODLE_20_STABLE \
+                            refs/remotes/origin/MOODLE_24_STABLE:refs/heads/MOODLE_24_STABLE
+# Discontinued 20140113 -   refs/remotes/origin/MOODLE_23_STABLE:refs/heads/MOODLE_23_STABLE
+# Discontinued 20140113 -   refs/remotes/origin/MOODLE_19_STABLE:refs/heads/MOODLE_19_STABLE
+# Discontinued 20130708 -   refs/remotes/origin/MOODLE_22_STABLE:refs/heads/MOODLE_22_STABLE
+# Discontinued 20130114 -   refs/remotes/origin/MOODLE_21_STABLE:refs/heads/MOODLE_21_STABLE
+# Discontinued 20120706 -   refs/remotes/origin/MOODLE_20_STABLE:refs/heads/MOODLE_20_STABLE
 
 output "${G}Done!${N}"
 exit 0
