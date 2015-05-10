b174b10948c86773e9b6338ed8687f796fd113a2
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2015-05-10 13:42:39 +0200

    NOBUG: Get rid of 26_STABLE and move 27_STABLE to security.

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@
 # it contains only references to the stable and security branches.
 
 # Current stable branches.
-STABLEBRANCHES=('MOODLE_27_STABLE' 'MOODLE_28_STABLE' 'MOODLE_29_STABLE')
+STABLEBRANCHES=('MOODLE_28_STABLE' 'MOODLE_29_STABLE')
 
 # Current security branches.
-SECURITYBRANCHES=('MOODLE_26_STABLE')
+SECURITYBRANCHES=('MOODLE_27_STABLE')
