moodlehq/mdlrelease

commit e2d7919a21156843663c8d3b2efd3283b7a1b532

NOBUG: Add "+" to on-demand rolls when needed.

bumpversions.php
@@ -151,6 +151,12 @@ function bump_version($path, $branch, $type, $rc = null, $date = null) {
             list($versionmajornew, $versionminornew) = bump_master_ensure_higher($versionmajornew, $versionminornew);
             $maturitynew = 'MATURITY_RC';
         } else if ($type === 'on-demand') {
+            // Add the + if missing (normally applies to post betas & rcs only,
+            // but it's not wrong to generalize it to any on-demand).
+            if (strpos($releasenew, '+') === false) {
+                // Add the +
+                $releasenew .= '+';
+            }
             list($versionmajornew, $versionminornew) = bump_master_ensure_higher($versionmajornew, $versionminornew);
         } else if ($type === 'on-sync') {
             $versionminornew++;

commit 21fa56bed7155b07fca6498e2b6e82aaf5140a19

The + saga continues..
I guess caused by e2d7919a very similar to 84171cbc..

When incrementing the version this time it got borked because our
release name is now 3.1dev+ and you ended up with:

diff --git a/version.php b/version.php
[...]
-$release  = '3.1dev+ (Build: 20160429)'; // Human-friendly version name
+$release  = '3.1+beta (Build: 20160504)'; // Human-friendly version name

-$branch   = '31';                       // This version's branch.
-$maturity = MATURITY_ALPHA;             // This version's maturity level.
+$branch   = '31+';                       // This version's branch.
+$maturity = MATURITY_BETA;             // This version's maturity lev

And misguided instructions telling you to tag that with the production
tag (ARGHGHHHHHHHHH!!!):

    git tag -a 'v3.1.0' -m 'MOODLE_31' 8ce725245d449154e181f7d26367b7221a5aa4e4

bumpversions.php
@@ -146,7 +146,7 @@ function bump_version($path, $branch, $type, $rc = null, $date = null) {
             }
             list($versionmajornew, $versionminornew) = bump_master_ensure_higher($versionmajornew, $versionminornew);
         } else if ($type === 'beta') {
-            $releasenew = preg_replace('#^(\d+.\d+) *(dev)#', '$1', $releasenew);
+            $releasenew = preg_replace('#^(\d+.\d+) *(dev\+?)#', '$1', $releasenew);
             $branchnew = str_replace('.', '', $releasenew);
             $releasenew .= 'beta';
             list($versionmajornew, $versionminornew) = bump_master_ensure_higher($versionmajornew, $versionminornew);